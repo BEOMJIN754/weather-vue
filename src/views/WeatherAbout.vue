bb9d9e5a78d0664bd317b31bd2dd17bdddaa09de
BEOMJIN754 <beomjin754@gmail.com>
2026-08-27 19:40:59 +0900

    환경변수 테스트

--- src/views/WeatherAbout.vue
+++ src/views/WeatherAbout.vue
@@ -11,6 +11,13 @@ const appTitle = inject('appTitle', 'Weather Dashboard')
       <strong>{{ appTitle }}</strong
       >is practice for vue3
     </p>
-    <p>Vue Router, Pinia, Axios, Element Plus 를 하나의 SPA 에서 연결합니다.</p>
+
+    <h3>주요 기능</h3>
+    <p>- 도시별 실시간 날씨 조회</p>
+    <p>- 도시 검색</p>
+    <p>- 섭씨/화씨 변환</p>
+    <p>- 상세 날씨 정보 제공 날씨</p>
+    - 버셀 환경변수 확인용 커밋
+    <p>정보를 API 를 활용하여 위트있게 가져옵니다.</p>
   </section>
 </template>
